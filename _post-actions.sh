1459f92ab597be7d79c7b568015f33d086ce980b
tehtbl <3999809+tehtbl@users.noreply.github.com>
2019-12-10 16:48:27 +0100

    updating font installing

--- _post-actions.sh
+++ _post-actions.sh
@@ -14,30 +14,18 @@ if [[ -f "${HOME}/.fonts/PowerlineSymbols.otf" ]];
 then
   echo "Fonts already installed"
 else
+  mkdir -p "${HOME}"/.fonts
+  mkdir -p "${HOME}"/.config/fontconfig/conf.d
+  mkdir -p "${HOME}"/.local/share/fonts
 
-  # Mac OSX specific font installations
-  #-------------------------------------------------------------------------------
-  if [[ "$OSTYPE" =~ ^darwin ]]; then
-    echo "TODO: install fonts manually!"
-  fi
+  wget -O /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip
+  unzip /tmp/Hack.zip -d "${HOME}"/.local/share/fonts
 
-  # Linux specific font installations
-  #-------------------------------------------------------------------------------
-  if [[ "$OSTYPE" =~ ^linux ]]; then
-    mkdir -p "${HOME}"/.fonts
-    mkdir -p "${HOME}"/.config/fontconfig/conf.d
-    mkdir -p "${HOME}"/.local/share/fonts
+  # TODO: check if still needed...
+  wget -P "${HOME}/.fonts" https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf &> /dev/null
+  wget -P "${HOME}/.config/fontconfig/conf.d/" https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf &> /dev/null
 
-    wget -O /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip
-    unzip /tmp/Hack.zip -d "${HOME}"/.local/share/fonts
-
-    # TODO: check if needed anymore!
-    wget -P "${HOME}/.fonts" https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf &> /dev/null
-    wget -P "${HOME}/.config/fontconfig/conf.d/" https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf &> /dev/null
-
-    fc-cache -vf "${HOME}/.fonts/" &> /dev/null
-  fi
-
+  fc-cache -f &> /dev/null
 fi
 
 if [[ -d "${HOME}/.tmux/plugins/tmux-plugin-sysstat" ]];
